77bea87025642f18345c2d196c817632d184993b
Prathmesh Pathak <pprathamesh1594@gmail.com>
2018-04-23 23:09:21 +0530

    add till hello_name.py

---
+++ CodingBat/String-1/extre_end.py
@@ -0,0 +1,7 @@
+# Given a string, return a new string made of 3 copies of the last 2
+# chars of the original string, the string length will be atleast 2
+
+def extra_end(strn):
+    return strn[-2:] * 3
+
+    
---
+++ CodingBat/String-1/first_two.py
@@ -0,0 +1,10 @@
+#GIven a string, return a string made of its first two chars, so the
+# String "Hello" yeids "He".
+# If the string is shorter than 2, return whatever there is,
+# so "X" yeils "X", and empty string "" yeilds the
+# empty string "".
+
+def first_two(strn):
+    return strn[:2]
+
+    
