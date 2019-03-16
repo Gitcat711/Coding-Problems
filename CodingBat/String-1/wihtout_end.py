481f90ec44baf2ba051d458bd3bfff0e4259f1c4
Pratik-sys <pithore.pratik@gmail.com>
2019-03-16 16:23:12 +0530

    fix merge conflict
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- CodingBat/String-1/wihtout_end.py
+++ CodingBat/String-1/wihtout_end.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-# Given a string, return a version without the first and last char, so "Hello"
-=======
 # Given a string, return a version without the first and last char, so "Hello" 
->>>>>>> master
 # yields "ell". The string length will be at least 2.
 
 def without_end(str):
